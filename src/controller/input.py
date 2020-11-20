9c41c6fba571ad38a2174780d998936de46317e6
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-11-20 22:35:05 +0700

    create input processor

---
+++ src/controller/input.py
@@ -0,0 +1,12 @@
+def process_input(file):
+    data = open(file).readlines()
+    size = int(data[0])
+    bombs_count = int(data[1])
+    coords = []
+    for i in range(bombs_count):
+        x, y = map(int, data[i+2].split(", "))
+        coords.append((x, y))
+    return size, coords
+
+if __name__ == "__main__":
+    print(process_input("tests/tc1.txt"))
